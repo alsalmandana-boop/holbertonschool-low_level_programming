29f24713bb7c846da5530c0f5a6148112ff10069
Dana <dana@holbertonstudents.com>
2026-07-10 16:22:50 +0300

    Print lowercase alphabet in reverse order

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+
+	while (c >='a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
