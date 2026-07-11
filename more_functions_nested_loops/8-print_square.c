7e0c670b31855d4f103ec4c940c0deb7a71ca014
Dana <dana@holbertonstudents.com>
2026-07-11 17:25:14 +0300

    add code for 8-print_square.c and update main.h to include the function prototype for print_square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of the square
+ *
+ * Return: nothing
+ */
+void print_square(int size)
+{
+	int row;
+	int col;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+}
