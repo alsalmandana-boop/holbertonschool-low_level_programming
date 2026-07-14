1b85e3295148b90a35b414f2c5a5754e5dd793cc
Dana <dana@holbertonstudents.com>
2026-07-14 19:13:23 +0300

    add code to benchmark instrumentation lab

--- benchmarking/instrumentation_lab.c
+++ benchmarking/instrumentation_lab.c
@@ -50,40 +50,55 @@ static unsigned long reduce_checksum(void)
     return sum;
 }
 
+
 int main(void)
 {
-    unsigned long checksum;
+	unsigned long checksum;
+	clock_t total_start;
+	clock_t total_end;
+	clock_t build_start;
+	clock_t build_end;
+	clock_t process_start;
+	clock_t process_end;
+	clock_t reduce_start;
+	clock_t reduce_end;
+	double total_seconds;
+	double build_seconds;
+	double process_seconds;
+	double reduce_seconds;
 
-/* متغيرات الوقت */
-    clock_t total_start;
-    clock_t total_end;
-    clock_t build_start;
-    clock_t build_end;
-    clock_t process_start;
-    clock_t process_end;
-    clock_t reduce_start;
-    clock_t reduce_end;
+	total_start = clock();
 
-    /* متغيرات الزمن بالثواني */
-    double total_seconds;
-    double build_seconds;
-    double process_seconds;
-    double reduce_seconds;
+	build_start = clock();
+	build_dataset();
+	build_end = clock();
 
+	process_start = clock();
+	process_dataset();
+	process_end = clock();
 
-    build_dataset();
-    process_dataset();
-    checksum = reduce_checksum();
+	reduce_start = clock();
+	checksum = reduce_checksum();
+	reduce_end = clock();
 
-    if (checksum == 0ul)
-        printf("impossible\n");
+	total_end = clock();
 
-    /* Required output (exact format, no extra lines):
-     * TOTAL seconds: <float>
-     * BUILD_DATA seconds: <float>
-     * PROCESS seconds: <float>
-     * REDUCE seconds: <float>
-     */
+	total_seconds = (double)(total_end - total_start) /
+		(double)CLOCKS_PER_SEC;
+	build_seconds = (double)(build_end - build_start) /
+		(double)CLOCKS_PER_SEC;
+	process_seconds = (double)(process_end - process_start) /
+		(double)CLOCKS_PER_SEC;
+	reduce_seconds = (double)(reduce_end - reduce_start) /
+		(double)CLOCKS_PER_SEC;
 
-    return 0;
+	if (checksum == 0ul)
+		printf("impossible\n");
+
+	printf("TOTAL seconds: %.6f\n", total_seconds);
+	printf("BUILD_DATA seconds: %.6f\n", build_seconds);
+	printf("PROCESS seconds: %.6f\n", process_seconds);
+	printf("REDUCE seconds: %.6f\n", reduce_seconds);
+
+	return (0);
 }
