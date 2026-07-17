5f1e1142b4023600ffdb860b28b81c445e67a991
Dana <dana@holbertonstudents.com>
2026-07-17 15:47:57 +0300

    add new code for 5-rev_string.c and update main.h to include rev_string function prototype

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+    int length;
+    int left;
+    int right;
+    char temp;
+
+    length = 0;
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+    left = 0;
+    right = length - 1;
+    while (left < right)
+    {
+        temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
+}
