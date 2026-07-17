f5189fb1b8f43fa1394c312196e7bda20fc334e5
Dana <dana@holbertonstudents.com>
2026-07-17 19:01:57 +0300

    add code in 7-puts and modifide

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int length;
+	int count;
+
+	length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		count = length / 2;
+	}
+	else
+	{
+		count = (length - 1) / 2;
+	}
+
+	while (str[count] != '\0')
+	{
+		_putchar(str[count]);
+		count++;
+	}
+
+	_putchar('\n');
+}
