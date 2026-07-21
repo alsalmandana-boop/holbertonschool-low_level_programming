4ff293879883913c0eaceb41b907ca44b59b0be5
Dana <dana@holbertonstudents.com>
2026-07-21 14:46:32 +0300

    modified file as betty styile

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,34 +11,33 @@
 
 char *_strdup(char *str)
 {
-    unsigned int length;
-    unsigned int i;
-    char *copy_str;
+	unsigned int length;
+	unsigned int i;
+	char *copy_str;
 
-    if (str == NULL)
-    {
-    return (NULL);
-    }
+	if (str == NULL)
+	{
+	return (NULL);
+	}
 
-    length = 0; 
-    while (str[length] != '\0')
-    {
-    length++;
-    }
+	length = 0;
+	while (str[length] != '\0')
+	{
+	length++;
+	}
 
-   copy_str = malloc (sizeof(char) * (length + 1));
+	copy_str = malloc(sizeof(char) * (length + 1));
 
-   if (copy_str == NULL)
-   {
-    return(NULL);
-   }
-    i = 0;
-    while (i < length)
-    {
-    copy_str[i] = str[i];
-    i++;
-    }
-    
-   
-   return(copy_str);
+	if (copy_str == NULL)
+	{
+	return (NULL);
+	}
+	i = 0;
+
+	while (i < length)
+	{
+	copy_str[i] = str[i];
+	i++;
+	}
+	return (copy_str);
 }
