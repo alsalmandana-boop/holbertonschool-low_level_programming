94d31b0f897a079f9e5b192d94bbc10fe65dbda2
Dana <dana@holbertonstudents.com>
2026-07-22 13:11:43 +0300

    commit the file and update it with betty styile

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,59 +8,42 @@
  *
  * Return: pointer to the concatenated string, or NULL
  */
-
 char *str_concat(char *s1, char *s2)
 {
-unsigned int len1;
-unsigned int len2;
-unsigned int sum_s;
-unsigned int i;
-unsigned int j;
-char *str_cat;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *str_cat;
 
-i = 0;
-j = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-if (s1 == NULL)
-	s1 = "";
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-if (s2 == NULL)
-	s2 = "";
+	str_cat = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (str_cat == NULL)
+		return (NULL);
 
-len1 = 0;
-while (s1[len1] != '\0')
-{
-    ++len1;
-}
+	while (s1[i] != '\0')
+	{
+		str_cat[i] = s1[i];
+		i++;
+	}
 
-len2 = 0;
-while (s2[len2] != '\0')
-{
-    ++len2;
-}
+	while (s2[j] != '\0')
+	{
+		str_cat[i] = s2[j];
+		i++;
+		j++;
+	}
 
-sum_s = len1 + len2;
+	str_cat[i] = '\0';
 
-str_cat = malloc(sizeof(char) * (sum_s + 1));
-if (str_cat == NULL)
-	return (NULL);
-
-
-while (s1[i] != '\0')
-{
-    str_cat[i] = s1[i];
-    i++;
-}
-
-
-while (s2[j] != '\0')
-{
-    str_cat[i] = s2[j];
-    i++;
-    j++;
-}
-
-str_cat[i] = '\0';
-
-return(str_cat);
+	return (str_cat);
 }
