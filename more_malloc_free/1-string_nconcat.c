971785f2dd94d7fef551533bd698b02263aa7630
Dana <dana@holbertonstudents.com>
2026-07-23 14:32:41 +0300

    commit file of code that's print nconcatnate strings

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to copy from s2
+ *
+ * Return: pointer to the new string, or NULL on failure
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1;
+	unsigned int len2;
+	unsigned int i;
+	unsigned int j;
+	char *new_str;
+
+	len1 = 0;
+	len2 = 0;
+	i = 0;
+	j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+
+	while (s2[len2] != '\0')
+		len2++;
+
+	if (n >= len2)
+		n = len2;
+
+	new_str = malloc(sizeof(char) * (len1 + n + 1));
+
+	if (new_str == NULL)
+		return (NULL);
+
+	while (s1[i] != '\0')
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+
+	while (j < n)
+	{
+		new_str[i] = s2[j];
+		i++;
+		j++;
+	}
+
+	new_str[i] = '\0';
+
+	return (new_str);
+}
