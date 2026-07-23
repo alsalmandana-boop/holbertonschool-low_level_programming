46d4d58024524ddc60f1b7c29cbf0e06d5bf6435
Dana <dana@holbertonstudents.com>
2026-07-23 12:02:08 +0300

    commit a free_grid code in 4-free_grid file and main.h

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - frees a two dimensional grid
+ * @grid: pointer to the grid
+ * @height: number of rows
+ *
+ * Return: Nothing
+ */
+void free_grid(int **grid, int height)
+{
+    int row;
+    
+    row = 0;
+    while (row < height)
+    {
+        free(grid[row]);
+        row++;
+    }
+    free(grid);
+}
