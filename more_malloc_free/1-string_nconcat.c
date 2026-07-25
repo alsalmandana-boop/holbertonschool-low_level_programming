ab0ac582fd0df4f05591e8b0d9de7f2afab3c11d
Dana <dana@holbertonstudents.com>
2026-07-25 13:26:34 +0300

    delete the addtion Lines

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -7,54 +7,32 @@
  * @s2: second string
  * @n: number of bytes to copy from s2
  *
- * Return: pointer to the new string, or NULL on failure
+ * Return: pointer to the new string, or NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1;
-	unsigned int len2;
-	unsigned int i;
-	unsigned int j;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 	char *new_str;
-
-	len1 = 0;
-	len2 = 0;
-	i = 0;
-	j = 0;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
 	while (s1[len1] != '\0')
 		len1++;
-
 	while (s2[len2] != '\0')
 		len2++;
-
 	if (n >= len2)
 		n = len2;
 
 	new_str = malloc(sizeof(char) * (len1 + n + 1));
-
 	if (new_str == NULL)
 		return (NULL);
 
 	while (s1[i] != '\0')
-	{
-		new_str[i] = s1[i];
-		i++;
-	}
-
+		new_str[i] = s1[i], i++;
 	while (j < n)
-	{
-		new_str[i] = s2[j];
-		i++;
-		j++;
-	}
-
+		new_str[i++] = s2[j++];
 	new_str[i] = '\0';
 
 	return (new_str);
