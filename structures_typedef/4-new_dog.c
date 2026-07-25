c12c6499b005ffc731c058fd2512277869820024
Dana <dana@holbertonstudents.com>
2026-07-25 22:36:36 +0300

    delete the Lines after 40 and rewrite the code

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -6,59 +6,36 @@
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
- *
- * Return: pointer to the new dog, or NULL on failure
+ * Return: pointer to the new dog, or NULL
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	unsigned int name_len;
-	unsigned int owner_len;
-	unsigned int i;
-
-	name_len = 0;
-	owner_len = 0;
+	unsigned int name_len = 0, owner_len = 0, i = 0;
 
 	while (name[name_len] != '\0')
 		name_len++;
-
 	while (owner[owner_len] != '\0')
 		owner_len++;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-
-	dog->name = malloc(sizeof(char) * (name_len + 1));
+	dog->name = malloc(name_len + 1);
 	if (dog->name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-
-	dog->owner = malloc(sizeof(char) * (owner_len + 1));
+		return (free(dog), NULL);
+	dog->owner = malloc(owner_len + 1);
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-
-	i = 0;
 	while (i <= name_len)
-	{
-		dog->name[i] = name[i];
-		i++;
-	}
-
+		dog->name[i] = name[i], i++;
 	i = 0;
 	while (i <= owner_len)
-	{
-		dog->owner[i] = owner[i];
-		i++;
-	}
-
+		dog->owner[i] = owner[i], i++;
 	dog->age = age;
-
 	return (dog);
 }
