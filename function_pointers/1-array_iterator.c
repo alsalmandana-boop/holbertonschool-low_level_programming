8e7d07fc61e11c0a8465aa6cf98369b494f31571
Dana <dana@holbertonstudents.com>
2026-08-01 15:01:36 +0300

    add new laibrary

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,11 +1,10 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
- * array_iterator - executes a function on each element of an array
+ * array_iterator - executes a function on each array element
  * @array: array of integers
- * @size: size of the array
- * @action: function pointer
+ * @size: number of elements in the array
+ * @action: function to execute on each element
  *
  * Return: Nothing
  */
