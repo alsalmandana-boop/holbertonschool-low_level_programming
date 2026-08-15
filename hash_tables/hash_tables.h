37f216bb4225460cd35c881a5fb983cc0e0769ee
Dana <dana@holbertonstudents.com>
2026-08-15 13:35:31 +0300

    Add function that computes a key index

---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,33 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: Key string
+ * @value: Value associated with the key
+ * @next: Pointer to the next node
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table data structure
+ * @size: Size of the array
+ * @array: Array of pointers to hash nodes
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key,
+	unsigned long int size);
+
+#endif /* HASH_TABLES_H */
