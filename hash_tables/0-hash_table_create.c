93fa9914d5a897cfc9ba2774e619919f2a435a3c
Dana <dana@holbertonstudents.com>
2026-08-15 13:57:27 +0300

    Add function that computes a key index

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,4 +1,32 @@
- hash_table_t *hash_table_create(unsigned long int size)
-{
+#include <stdlib.h>
+#include "hash_tables.h"
 
+/**
+ * hash_table_create - Creates a hash table
+ * @size: Size of the hash table array
+ *
+ * Return: Pointer to the new hash table, or NULL on failure
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	if (size == 0)
+		return (NULL);
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	table->size = size;
+
+	return (table);
 }
+
