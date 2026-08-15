89cc8ad6ce963ee84bde5e10e3f7b380a56ee5bc
Dana <dana@holbertonstudents.com>
2026-08-15 15:05:14 +0300

    Refactor hash table set to meet Betty style

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key
+ * @ht: Pointer to the hash table
+ * @key: Key to search for
+ *
+ * Return: Value associated with the key, or NULL if not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return (NULL);
+
+	if (key == NULL || key[0] == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+
+		node = node->next;
+	}
+
+	return (NULL);
+}
