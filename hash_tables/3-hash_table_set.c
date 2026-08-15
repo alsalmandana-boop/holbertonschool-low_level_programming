2c7eba252c35799436410a665f67a4c4df58f7cb
Dana <dana@holbertonstudents.com>
2026-08-15 14:41:15 +0300

    Refactor hash table set to meet Betty style

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,6 +3,61 @@
 #include "hash_tables.h"
 
 /**
+ * update_value - Updates the value of an existing hash node
+ * @node: Pointer to the existing node
+ * @value: New value to store
+ *
+ * Return: 1 on success, or 0 on failure
+ */
+static int update_value(hash_node_t *node, const char *value)
+{
+	char *new_value;
+
+	new_value = strdup(value);
+	if (new_value == NULL)
+		return (0);
+
+	free(node->value);
+	node->value = new_value;
+
+	return (1);
+}
+
+/**
+ * create_hash_node - Creates a new hash node
+ * @key: Key to duplicate
+ * @value: Value to duplicate
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
+static hash_node_t *create_hash_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
  * hash_table_set - Adds or updates an element in a hash table
  * @ht: Pointer to the hash table
  * @key: Key associated with the value
@@ -16,11 +71,9 @@ int hash_table_set(hash_table_t *ht, const char *key,
 	unsigned long int index;
 	hash_node_t *node;
 	hash_node_t *new_node;
-	char *new_value;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0)
 		return (0);
-
 	if (key == NULL || key[0] == '\0' || value == NULL)
 		return (0);
 
@@ -30,38 +83,13 @@ int hash_table_set(hash_table_t *ht, const char *key,
 	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
-		{
-			new_value = strdup(value);
-			if (new_value == NULL)
-				return (0);
-
-			free(node->value);
-			node->value = new_value;
-
-			return (1);
-		}
-
+			return (update_value(node, value));
 		node = node->next;
 	}
 
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = create_hash_node(key, value);
 	if (new_node == NULL)
 		return (0);
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
