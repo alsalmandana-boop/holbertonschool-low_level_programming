ac8ddbb87f7f7b03982b88b8ab65162bf7093ba3
Dana <dana@holbertonstudents.com>
2026-08-18 13:31:05 +0300

    commit task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters read and printed, or 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t nread, nwritten;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	nread = read(fd, buf, letters);
+	if (nread == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	nwritten = write(STDOUT_FILENO, buf, nread);
+	if (nwritten == -1 || nwritten != nread)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	free(buf);
+	close(fd);
+
+	return (nwritten);
+}
+
